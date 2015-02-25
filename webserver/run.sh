2b4d434265bc608a7bf1d1edfc20d646491be4c5
ra1f <ralf.dueerkop@gmail.com>
2015-02-25 19:01:52 +0100

    blah

--- webserver/run.sh
+++ webserver/run.sh
@@ -3,5 +3,5 @@
 LOGDIR=$1
 MY_IP=$(ip addr show dev eth0 scope global | grep inet | awk -F'[/ ]' '{print $6}')
 
-echo "hello world" > $LOGDIR/${MY_IP}.log
+groovy /opt/webserver/webServer.groovy 2> $LOGDIR/${MY_IP}.log
 
